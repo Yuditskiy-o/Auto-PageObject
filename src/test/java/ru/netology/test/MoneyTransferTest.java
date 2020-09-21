f7a0d1bb387f90c3c4470149069f6cfb5db3cf78
Yuditskiy-o <Komekadze@gmail.com>
2020-09-22 01:33:36 +0700

    Prepared data for HW

---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,16 @@
+package ru.netology.test;
+
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPageV1;
+import ru.netology.web.page.LoginPageV2;
+import ru.netology.web.page.LoginPageV3;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+
+}
+
